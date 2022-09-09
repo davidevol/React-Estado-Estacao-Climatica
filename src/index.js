d60346a90bf55a1d29daf5fd1028e75484ae43ee
davideploy <deehzi@pm.me>
2022-09-09 13:17:38 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+class App extends React.Component {
+
+  constructor (props){
+    super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icone: null
+    }
+  }
+
+  obterEstacao = (data, latitude) => {
+    const anoAtual = data.getFullYear()
+    //new Date(ano,mês(0 a 11), dia (1, 31))
+    //21/06
+    const d1 = new Date(anoAtual, 5, 21)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //22/12
+    const d3 = new Date(anoAtual, 11, 22)
+    //21/03
+    const d4 = new Date(anoAtual, 2, 21)
+    const sul = latitude < 0
+    if (data >= d1 && data < d2) 
+      return sul ? 'Inverno' : 'Verão'
+    if (data >= d2 && data < d3)
+      return sul ? 'Primavera' : 'Outono'
+    if (data >= d3 && data < d4)
+      return sul ? 'Verão' : 'Inverno'
+    return sul ? 'Outono' : 'Primavera'
+  }
+
+  render(){
+    return (
+      <div>
+        Meu app
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
